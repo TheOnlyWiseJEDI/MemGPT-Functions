f85c6e432ac8c07fd26d1b5db744df3b9b767614
TheOnlyWiseJEDI <123831682+TheOnlyWiseJEDI@users.noreply.github.com>
2023-12-25 20:45:45 -0500

    Update send_to_chrome.py

--- Send_To_Chrome/send_to_chrome.py
+++ Send_To_Chrome/send_to_chrome.py
@@ -1,1 +1,18 @@
+import webbrowser
 
+def send_to_chrome(self, url: str):
+    """
+    Send a URL to the Chrome Browser
+    
+    Args:
+        url: (str)  The URL to send to Chrome
+        
+    Returns:
+        str: Reply confirming success
+    """
+
+    print(f"Sending URL to Chrome: {url}")
+
+    webbrowser.open(url) 
+
+    return "Success! Browser process launched."
